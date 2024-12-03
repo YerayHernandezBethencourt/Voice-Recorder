ea2bf002bd698ea0b1dd4bc9f0e0efe3ac1efb04
Yeray <yera_hb@hotmail.es>
2024-12-03 18:27:35 +0000

    First voice recorder
    Generate a 10 seconds audio record

---
+++ main.py
@@ -0,0 +1,10 @@
+import sounddevice
+from scipy.io.wavfile import write
+
+fs = 44100 # sample rate
+seconds = 10
+print("recording...")
+record_voice = sounddevice.rec(int(seconds * fs), samplerate=fs, channels=2)
+sounddevice.wait()
+write("output.wav", fs, record_voice)
+print("done recording")
