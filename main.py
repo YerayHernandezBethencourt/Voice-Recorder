9bf7dd609bdb59716527e8e8ca552fe089caca1e
Yeray <yera_hb@hotmail.es>
2024-12-03 18:30:45 +0000

    Update main.py
    Adding error management and upgrading coments

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import sounddevice
 from scipy.io.wavfile import write
 
-fs = 44100 # sample rate
+fs = 44100 # Sample rate
 seconds = 10
-print("recording...")
-record_voice = sounddevice.rec(int(seconds * fs), samplerate=fs, channels=2)
-sounddevice.wait()
+
+try:
+    print("recording...")
+    # Recording
+    record_voice = sounddevice.rec(int(seconds * fs), samplerate=fs, channels=2)
+    sounddevice.wait()  # Wait until recording is finished
+    print("finished recording")
+except:
+    print("error recording")
+
 write("output.wav", fs, record_voice)
 print("done recording")
